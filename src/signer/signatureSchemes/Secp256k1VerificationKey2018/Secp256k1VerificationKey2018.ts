a664575372e59d5cc18b43373c2a031798572c65
RJ <27843274+rjchow@users.noreply.github.com>
2021-02-22 14:39:10 +0800

    wip

--- src/signer/signatureSchemes/Secp256k1VerificationKey2018/Secp256k1VerificationKey2018.ts
+++ src/signer/signatureSchemes/Secp256k1VerificationKey2018/Secp256k1VerificationKey2018.ts
@@ -1,11 +1,27 @@
-import { Wallet, utils } from "ethers";
-import { SigningFunction, SigningKey } from "../../../types";
+import { Wallet, utils, ethers } from "ethers";
+import { SigningFunction, SigningKey, SigningKeyT, SigningOptions } from "../../../types";
 
 export const name = "Secp256k1VerificationKey2018";
 
-export const sign: SigningFunction = async (message: string, key: SigningKey, options = {}) => {
-  const wallet = new Wallet(key.private);
-  if (!key.public.toLowerCase().includes(wallet.address.toLowerCase()))
-    throw new Error(`Private key is wrong for ${key.public}`);
-  return wallet.signMessage(options.signAsString ? message : utils.arrayify(message));
-};
+export async function sign(message: string, key: SigningKey, options?: SigningOptions): Promise<string>;
+export async function sign(message: string, signer: ethers.Signer, options?: SigningOptions): Promise<string>;
+
+export async function sign(
+  message: string,
+  keyOrSigner: SigningKey | ethers.Signer,
+  options: SigningOptions = {}
+): Promise<string> {
+  let signer: ethers.Signer;
+  if (SigningKeyT.guard(keyOrSigner)) {
+    const wallet = new Wallet(keyOrSigner.private);
+    if (!keyOrSigner.public.toLowerCase().includes(wallet.address.toLowerCase())) {
+      throw new Error(`Private key is wrong for ${keyOrSigner.public}`);
+    }
+    signer = wallet;
+  } else if (keyOrSigner instanceof ethers.Signer) {
+    signer = keyOrSigner;
+  } else {
+    throw new Error(`Either a keypair or ethers.js Signer must be provided`);
+  }
+  return signer.signMessage(options.signAsString ? message : utils.arrayify(message));
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,16 @@
+import { ethers } from "ethers";
+import { Record, Static, String } from "runtypes";
+
 export interface SigningOptions {
   signAsString?: boolean;
+  signer?: ethers.Signer;
 }
 
-export interface SigningKey {
-  private: string;
-  public: string;
-}
+export const SigningKeyT = Record({
+  private: String,
+  public: String,
+});
+
+export type SigningKey = Static<typeof SigningKeyT>;
 
 export type SigningFunction = (message: string, key: SigningKey, options?: SigningOptions) => Promise<string>;
