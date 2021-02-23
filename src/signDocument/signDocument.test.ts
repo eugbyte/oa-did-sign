0e1c590fa15c9d202bc8afad590d26d0eeefea6b
Sia Chen Ren <siachenren@gmail.com>
2021-02-23 11:37:45 +0800

    feat: v3 did sign

--- src/signDocument/signDocument.test.ts
+++ src/signDocument/signDocument.test.ts
@@ -1,42 +1,83 @@
-import { signDocument } from ".";
-import unsignedDocument from "../../test/fixtures/unsignedDocument.json";
+import { v2, v3 } from "@govtechsg/open-attestation";
+import { signDocument, SUPPORTED_SIGNING_ALGORITHM } from ".";
+import rawWrappedDocumentV2 from "../../test/fixtures/v2/did-wrapped.json";
+import rawWrappedDocumentV3 from "../../test/fixtures/v3/did-wrapped.json";
+
+const wrappedDocumentV2 = rawWrappedDocumentV2 as v2.WrappedDocument;
+const wrappedDocumentV3 = rawWrappedDocumentV3 as v3.WrappedDocument;
 
 describe("signDocument", () => {
-  it("should sign a document", async () => {
-    const { proof } = await signDocument(
-      unsignedDocument as any,
-      "Secp256k1VerificationKey2018",
-      "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
-      "0x497c85ed89f1874ba37532d1e33519aba15bd533cdcb90774cc497bfe3cde655"
-    );
-    expect(proof.length).toBe(1);
-    expect(proof[0].proofPurpose).toBe("assertionMethod");
-    expect(proof[0].type).toBe("OpenAttestationSignature2018");
-    expect(proof[0].verificationMethod).toBe("did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller");
-    expect(proof[0].signature).toBe(
-      "0xff0227ce8400a17a2d80073a95fd895f4fed0011954c90eef389bc618087a4b36ed958775420d122e9a6764c6ffe9d3302d4f45fb065d5e962c3572d3872f31a1b"
-    );
+  describe("v2", () => {
+    it("should sign a document", async () => {
+      const { proof } = await signDocument(
+        wrappedDocumentV2,
+        SUPPORTED_SIGNING_ALGORITHM.Secp256k1VerificationKey2018,
+        "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+        "0x497c85ed89f1874ba37532d1e33519aba15bd533cdcb90774cc497bfe3cde655"
+      );
+      expect(proof.length).toBe(1);
+      expect(proof[0].proofPurpose).toBe("assertionMethod");
+      expect(proof[0].type).toBe("OpenAttestationSignature2018");
+      expect(proof[0].verificationMethod).toBe("did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller");
+      expect(proof[0].signature).toBe(
+        "0xff0227ce8400a17a2d80073a95fd895f4fed0011954c90eef389bc618087a4b36ed958775420d122e9a6764c6ffe9d3302d4f45fb065d5e962c3572d3872f31a1b"
+      );
+    });
+
+    it("should sign a document which was previously signed", async () => {
+      const signedDocument = await signDocument(
+        wrappedDocumentV2,
+        SUPPORTED_SIGNING_ALGORITHM.Secp256k1VerificationKey2018,
+        "did:ethr:0xb6De3744E1259e1aB692f5a277f053B79429c5a2#controller",
+        "0x812269266b34d2919f737daf22db95f02642f8cdc0ca673bf3f701599f4971f5"
+      );
+      const { proof } = await signDocument(
+        signedDocument,
+        SUPPORTED_SIGNING_ALGORITHM.Secp256k1VerificationKey2018,
+        "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+        "0x497c85ed89f1874ba37532d1e33519aba15bd533cdcb90774cc497bfe3cde655"
+      );
+      expect(proof.length).toBe(2);
+      expect(proof[1].proofPurpose).toBe("assertionMethod");
+      expect(proof[1].type).toBe("OpenAttestationSignature2018");
+      expect(proof[1].verificationMethod).toBe("did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller");
+      expect(proof[1].signature).toBe(
+        "0xff0227ce8400a17a2d80073a95fd895f4fed0011954c90eef389bc618087a4b36ed958775420d122e9a6764c6ffe9d3302d4f45fb065d5e962c3572d3872f31a1b"
+      );
+    });
   });
 
-  it("should sign a document which was previously signed", async () => {
-    const signedDocument = await signDocument(
-      unsignedDocument as any,
-      "Secp256k1VerificationKey2018",
-      "did:ethr:0xb6De3744E1259e1aB692f5a277f053B79429c5a2#controller",
-      "0x812269266b34d2919f737daf22db95f02642f8cdc0ca673bf3f701599f4971f5"
-    );
-    const { proof } = await signDocument(
-      signedDocument,
-      "Secp256k1VerificationKey2018",
-      "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
-      "0x497c85ed89f1874ba37532d1e33519aba15bd533cdcb90774cc497bfe3cde655"
-    );
-    expect(proof.length).toBe(2);
-    expect(proof[1].proofPurpose).toBe("assertionMethod");
-    expect(proof[1].type).toBe("OpenAttestationSignature2018");
-    expect(proof[1].verificationMethod).toBe("did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller");
-    expect(proof[1].signature).toBe(
-      "0xff0227ce8400a17a2d80073a95fd895f4fed0011954c90eef389bc618087a4b36ed958775420d122e9a6764c6ffe9d3302d4f45fb065d5e962c3572d3872f31a1b"
-    );
+  describe("v3", () => {
+    it("should sign a document", async () => {
+      const { proof } = await signDocument(
+        wrappedDocumentV3,
+        SUPPORTED_SIGNING_ALGORITHM.Secp256k1VerificationKey2018,
+        "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+        "0x497c85ed89f1874ba37532d1e33519aba15bd533cdcb90774cc497bfe3cde655"
+      );
+      expect(Object.keys(proof).length).toBe(9);
+      expect(proof.key).toBe("did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller");
+      expect(proof.signature).toBe(
+        "0x3ce62bb2d6a68f15fabe653dbb71edfaf1e2a00c71c98f71801f3a7438ae477a4cf3dbefaff9359b2d2b97473909ba4b19e9d2ac0c735cec926da0d1025af07b1c"
+      );
+    });
+
+    it("should throw error if a document was previously signed", async () => {
+      const signedDocument = await signDocument(
+        wrappedDocumentV3,
+        SUPPORTED_SIGNING_ALGORITHM.Secp256k1VerificationKey2018,
+        "did:ethr:0xb6De3744E1259e1aB692f5a277f053B79429c5a2#controller",
+        "0x812269266b34d2919f737daf22db95f02642f8cdc0ca673bf3f701599f4971f5"
+      );
+
+      await expect(
+        signDocument(
+          signedDocument,
+          SUPPORTED_SIGNING_ALGORITHM.Secp256k1VerificationKey2018,
+          "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+          "0x497c85ed89f1874ba37532d1e33519aba15bd533cdcb90774cc497bfe3cde655"
+        )
+      ).rejects.toThrowErrorMatchingInlineSnapshot(`"Document has been signed"`);
+    });
   });
 });
--- src/signDocument/signDocument.ts
+++ src/signDocument/signDocument.ts
@@ -1,23 +1,19 @@
-import {
-  WrappedDocument,
-  SignedWrappedDocument,
-  ProofType,
-  utils,
-  ProofPurpose,
-  v2,
-} from "@govtechsg/open-attestation";
+import { utils, v2, v3, ProofType, ProofPurpose } from "@govtechsg/open-attestation";
 import { SigningKey } from "src/types";
 import { sign } from "../signer";
 
-export const signDocument = async <T extends v2.OpenAttestationDocument>(
-  document: SignedWrappedDocument<T> | WrappedDocument<T>,
-  algorithm: string,
+export enum SUPPORTED_SIGNING_ALGORITHM {
+  Secp256k1VerificationKey2018 = "Secp256k1VerificationKey2018",
+}
+
+export const signV2Document = async <T extends v2.OpenAttestationDocument>(
+  document: v2.SignedWrappedDocument<T> | v2.WrappedDocument<T>,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
   publicKey: string,
   privateKey: string
-): Promise<SignedWrappedDocument<T>> => {
-  if (!utils.isWrappedV2Document(document)) throw new Error("Only v2 document is supported now");
-  const merkleRoot = `0x${document.signature.merkleRoot}`;
+): Promise<v2.SignedWrappedDocument<T>> => {
   const signingKey: SigningKey = { private: privateKey, public: publicKey };
+  const merkleRoot = `0x${document.signature.merkleRoot}`;
   const signature = await sign(algorithm, merkleRoot, signingKey);
   const proof = {
     type: ProofType.OpenAttestationSignature2018,
@@ -26,7 +22,55 @@ export const signDocument = async <T extends v2.OpenAttestationDocument>(
     verificationMethod: publicKey,
     signature,
   };
-  return utils.isSignedWrappedV2Document(document)
-    ? { ...document, proof: [...document.proof, proof] }
-    : { ...document, proof: [proof] };
+  return {
+    ...document,
+    proof: utils.isSignedWrappedV2Document(document) ? [...document.proof, proof] : [proof],
+  };
 };
+
+export const signV3Document = async <T extends v3.OpenAttestationDocument>(
+  document: v3.SignedWrappedDocument<T> | v3.WrappedDocument<T>,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  publicKey: string,
+  privateKey: string
+): Promise<v3.SignedWrappedDocument<T>> => {
+  if (utils.isSignedWrappedV3Document(document)) throw new Error("Document has been signed");
+  const signingKey: SigningKey = { private: privateKey, public: publicKey };
+  const merkleRoot = `0x${document.proof.merkleRoot}`;
+  const signature = await sign(algorithm, merkleRoot, signingKey);
+  const proof: v3.VerifiableCredentialProofSigned = {
+    ...document.proof,
+    key: publicKey,
+    signature,
+  };
+  return { ...document, proof };
+};
+
+export async function signDocument<T extends v3.OpenAttestationDocument>(
+  document: v3.SignedWrappedDocument<T> | v3.WrappedDocument<T>,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  publicKey: string,
+  privateKey: string
+): Promise<v3.SignedWrappedDocument<T>>;
+export async function signDocument<T extends v2.OpenAttestationDocument>(
+  document: v2.SignedWrappedDocument<T> | v2.WrappedDocument<T>,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  publicKey: string,
+  privateKey: string
+): Promise<v2.SignedWrappedDocument<T>>;
+export async function signDocument(
+  document: any,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  publicKey: string,
+  privateKey: string
+) {
+  switch (true) {
+    case utils.isWrappedV2Document(document):
+      return signV2Document(document, algorithm, publicKey, privateKey);
+    case utils.isWrappedV3Document(document):
+      return signV3Document(document, algorithm, publicKey, privateKey);
+    default:
+      // Unreachable code atm until utils.isWrappedV2Document & utils.isWrappedV3Document becomes more strict
+      throw new Error("Unsupported document type: Only OpenAttestation v2 & v3 documents can be signed");
+  }
+}
